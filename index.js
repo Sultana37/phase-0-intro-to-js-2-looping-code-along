36c8bdfe270220801c094d3a741e24a6ccc93738
Esrat <imranzaman.318@gmail.com>
2021-03-19 01:59:19 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const names=["Lisa","Kaitlin","Jan"];
+const celeb =["surprise"];
+function writeCards(names,celeb){
+    for (let i = 0; i < names.length; i++){
+        console.log(`Thank you ${names[i]}, for the wonderful ${celeb} gift`);
+        debugger;
+    }
+    return names;
+}
+writeCards(names,celeb);
+
+function countDown(number){
+let i = 0;
+while (number >= 0){
+    i++;
+    console.log({number});
+    number--;
+}
+countDown(10);
+countDown(4);
+
+
+}
